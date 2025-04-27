2be3586e4e28c8e9b5a93ff524160197162098b3
Eva <tlox_xaxaxa@mail.ru>
2025-04-27 14:31:37 +0300

    done

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import telebot
 from telebot import types
 
-BUTTONS = ["Новая задача", "Список дел"]
+BUTTONS = ["Новая задача👠", "Список дел📝"]
 todolist = []
 WEEK_BUTTONS = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
 TIME_START = 7
 TIME_END = 21
+TASK_BUTTON = ["✅","❌","🗓","🕒"]
 
 
 class Task:
@@ -26,11 +27,11 @@ def start(message):
     # markup.add(todolist_btn)
     # markup.add(newtask_btn)
     markup.row(newtask_btn, todolist_btn)
-    bot.send_message(message.chat.id, "Привет! Что нужно сделать?", reply_markup=markup)
+    bot.send_message(message.chat.id, "Привет!👋🏿 Что нужно сделать?", reply_markup=markup)
 
 
 def add_new_task(message):
-    bot.send_message(message.chat.id, "Введите название задачи")
+    bot.send_message(message.chat.id, "Введите название задачи✏")
 
 
 @bot.message_handler(commands=["newtask"])
@@ -39,12 +40,19 @@ def new_task(message):
 
 
 def print_todolist(message):
-    bot.send_message(message.chat.id, "Вот твой список дел: ")
-    text_list = ""
-    for i, task in enumerate(todolist):
-        text_list += f" задача №{i + 1}: {task.name}\n{task.day} {task.time}\n"
-    bot.send_message(message.chat.id, text_list)
+    bot.send_message(message.chat.id, "Вот твой список дел📅: ")
+    if not todolist:
+        bot.send_message(message.chat.id, "Ваш список пуст😶😶😶")
+    else:
+        for i, task in enumerate(todolist):
+            text = f" 📋задача №{i + 1}: {task.name}\n{task.day} {task.time}\n"
+            bot.send_message(message.chat.id, text)
 
+def task_keyboard():
+    keyboard = types.InlineKeyboardMarkup()
+    for i in TASK_BUTTON:
+        task_button = types.InlineKeyboardButton(i,callback_data=i)
+        keyboard.add(task_button)
 
 @bot.message_handler(commands=["todolist"])
 def list(message):
@@ -65,38 +73,40 @@ def answer(message):
     else:
         newtask = Task(message.text)
         todolist.append(newtask)
-        bot.send_message(message.chat.id, f"Добавлена задача \n {newtask.name}")
+        bot.send_message(message.chat.id, f"✏Добавлена задача \n {newtask.name}")
         keyboard = types.InlineKeyboardMarkup(row_width=2)
         keyboard_buttons = []
         for day in WEEK_BUTTONS:
             day_btn = types.InlineKeyboardButton(day, callback_data=day)
             keyboard_buttons.append(day_btn)
         keyboard.add(*keyboard_buttons)
-        bot.send_message(message.chat.id, "Выберите день начала задачи", reply_markup=keyboard)
+        bot.send_message(message.chat.id, "Выберите день начала задачи📆", reply_markup=keyboard)
         keyboard_time = types.InlineKeyboardMarkup(row_width=5)
         keyboard_buttons_time = []
         for time in range(TIME_START,TIME_END+1):
             time_text = f"{time}:00"
-            time_btn = types.InlineKeyboardButton(time_text,callback_data=time_text)
+            time_btn = types.InlineKeyboardButton(time_text,callback_data = f"time_select{time_text}")
             keyboard_buttons_time.append(time_btn)
         keyboard_time.add(*keyboard_buttons_time)
-        bot.send_message(message.chat.id,"Выберете время начала задачи",reply_markup=keyboard_time)
+        bot.send_message(message.chat.id,"Выберете время начала задачи🕰",reply_markup=keyboard_time)
 
 
-@bot.callback_query_handler(func=lambda call: True)
+@bot.callback_query_handler(func=lambda call: call.data in WEEK_BUTTONS)
 def day_select(call):
     if len(todolist) > 0:
         todolist[-1].day = call.data
     bot.edit_message_text(f"запланирована на {call.data}",call.message.chat.id, call.message.message_id)
     bot.answer_callback_query(call.id)
+    print("выбор дня")
 
 
-@bot.callback_query_handler(func=lambda call: True)
+@bot.callback_query_handler(func=lambda call: call.data[0:11] == "time_select")
 def time_select(call):
     if len(todolist) > 0:
-        todolist[-1].time = call.data
-    bot.edit_message_text(f"запланирована на {call.data}",call.message.chat.id,call.message.message_id)
-
+        todolist[-1].time = call.data[11:]
+    bot.edit_message_text(f"запланирована на {call.data[11:]}",call.message.chat.id,call.message.message_id)
+    bot.answer_callback_query(call.id)
+    print("выбор времени")
 
 
 
